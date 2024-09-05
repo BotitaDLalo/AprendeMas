f3900fdbfb4999bdada1ec640466c0d4da012e8c
BotitaDLalo <canciondfuego0@gmail.com>
2024-09-05 02:44:09 -0600

    Riverpod ProviderScope and authenticatio_provider file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:aprende_mas/config/router/router.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:aprende_mas/config/utils/app_theme.dart';
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(child: MainApp())
+  );
 }
 
 class MainApp extends StatelessWidget {
