4293a745347ba0179ece22d60651f8318e837353
BotitaDLalo <canciondfuego0@gmail.com>
2024-12-16 02:55:40 -0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:aprende_mas/config/environment/environmen.dart';
+import 'package:aprende_mas/config/environment/environment.dart';
 import 'package:aprende_mas/config/router/router.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:aprende_mas/config/utils/app_theme.dart';
+import 'package:aprende_mas/config/utils/packages.dart';
+import 'package:aprende_mas/config/services/firebase_configuration.dart';
 
-void main() async{
+void main() async {
   await Environment.initEnvironment();
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseConfiguration.initializeFCM();
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -14,7 +16,6 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final appRouter = ref.watch(goRouterProvider);
 
     return MaterialApp.router(
