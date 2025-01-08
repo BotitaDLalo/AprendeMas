59d877095bbaacadf4853b13cdd0fee7683011fa
BotitaDLalo <canciondfuego0@gmail.com>
2025-01-07 21:55:12 -0600

    Cambio en campos de tablas locales

--- lib/config/data/querys.dart
+++ lib/config/data/querys.dart
@@ -1,10 +1,17 @@
 class Querys {
   static List<String> querysCreateTables() => [
         "CREATE TABLE tbUsuarioActivo(UsuarioId INTEGER PRIMARY KEY, NombreUsuario TEXT, Correo TEXT, FechaLimiteActivo TEXT, Rol TEXT);",
-        "CREATE TABLE tbGruposAlumno(GrupoId INTEGER PRIMARY KEY, NombreGrupo TEXT, Descripcion TEXT);",
-        "CREATE TABLE tbMateriasAlumno(MateriaId INTEGER PRIMARY KEY, NombreMateria TEXT, Descripcion TEXT);",
-        "CREATE TABLE tbActividades(ActividadId INTEGER PRIMARY KEY, NombreActividad TEXT, Descripcion TEXT, FechaCreacion TEXT, FechaLimite TEXT, MateriaId);",
+
+        "CREATE TABLE tbActividades(ActividadId INTEGER PRIMARY KEY, NombreActividad TEXT, Descripcion TEXT, FechaCreacion TEXT, FechaLimite TEXT);",
+
+        "CREATE TABLE tbMaterias(MateriaId INTEGER PRIMARY KEY, NombreMateria TEXT, Descripcion TEXT, ActividadId INTEGER, FOREIGN KEY (ActividadId) REFERENCES tbActividades(ActividadId));",
+        
+        "CREATE TABLE tbGruposAlumno(GrupoAlumnoId INTEGER PRIMARY KEY, NombreGrupo TEXT, Descripcion TEXT, CodigoColor TEXT, MateriaId INTEGER, FOREIGN KEY (MateriaId) REFERENCES tbMaterias(MateriaId));",
+        
+        "CREATE TABLE tbMateriasAlumno(MateriaAlumnoId INTEGER PRIMARY KEY, NombreMateria TEXT, Descripcion TEXT, MateriaId INTEGER, FOREIGN KEY (MateriaId) REFERENCES tbMaterias(MateriaId));",
+        
         "CREATE TABLE tbEntregaActividades(EntregaId INTEGER PRIMARY KEY, FechaEntrega TEXT, UsuarioId INTEGER, ActividadId INTEGER);",
+        
         "CREATE TABLE tbNotificaciones(MensajeId TEXT, Titulo TEXT, Descripcion TEXT, FechaEnvio TEXT, Data TEXT, ImagenURL TEXT);",
       ];
 
@@ -12,6 +19,7 @@ class Querys {
         "DELETE FROM tbUsuarioActivo;",
         "DELETE FROM tbGruposAlumno;",
         "DELETE FROM tbMateriasAlumno;",
+        "DELETE FROM tbMaterias;",
         "DELETE FROM tbActividades;",
         "DELETE FROM tbEntregaActividades;",
         "DELETE FROM tbNotificaciones;"
