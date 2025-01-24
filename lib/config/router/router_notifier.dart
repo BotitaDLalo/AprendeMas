40e4c707ab62c8d3444751906f4099a6f53bfdfd
BotitaDLalo <canciondfuego0@gmail.com>
2025-01-23 21:43:24 -0600

    feat: Añade import de CatalogNames

--- lib/config/router/router_notifier.dart
+++ lib/config/router/router_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:aprende_mas/providers/authentication/auth_state.dart';
 import 'package:aprende_mas/providers/authentication/auth_state_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:aprende_mas/config/utils/catalog_names.dart';
 
 class RouterNotifier extends ChangeNotifier {
   final AuthStateNotifier _authStateNotifier;
