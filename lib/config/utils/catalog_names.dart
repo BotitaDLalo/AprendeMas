58624730f020df6cf81669b437c99b95302fddc9
BotitaDLalo <canciondfuego0@gmail.com>
2025-01-23 20:16:23 -0600

    refactor(config/utils): Añade enums provenientes de AuthState

--- lib/config/utils/catalog_names.dart
+++ lib/config/utils/catalog_names.dart
@@ -1,4 +1,19 @@
+enum AuthStatus { checking, authenticated, notAuthenticated }
+
+enum AuthenticatedType { auth, authGoogle, undefined }
+
+enum AuthGoogleStatus { checking, authenticated, notAuthenticated }
+
+enum RegisterStatus { registered, notRegistered }
+
+enum AuthConectionType { online, offline, unverified }
+
 class CatalogNames {
   String get getRoleTeacherName => "Docente";
   String get getRoleStudentName => "Alumno";
+  String get getKeyIdName => "id";
+  String get getKeyRoleName => "role";
+  String get getKeyTokenName => "token";
+  String get getKeyUserName => "username";
+  String get getKeyAuthTypeName => "authtype";
 }
