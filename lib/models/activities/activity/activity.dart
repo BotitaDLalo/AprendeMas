5c3a2a29a40bd0297519de2db8a202513026d480
BotitaDLalo <canciondfuego0@gmail.com>
2025-01-27 22:38:17 -0600

    feat(models/activity): Crea método para convertir list de la bd local a un tipo List<Activity>

--- lib/models/activities/activity/activity.dart
+++ lib/models/activities/activity/activity.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class Activity {
   final int actividadId;
   final String nombreActividad;
@@ -17,4 +19,22 @@ class Activity {
       required this.fechaCreacion,
       required this.fechaLimite,
       required this.materiaId});
+  static final DateFormat dateTimeFormat = DateFormat('yyyy-MM-ddTHH:mm:ss');
+  static List<Activity> queryToEntityActivity(
+      List<Map<String, Object?>> querylsActivities) {
+    final lsActivities = querylsActivities
+        .map(
+          (e) => Activity(
+              actividadId: e['ActividadId'] as int,
+              nombreActividad: e['NombreActividad'] as String,
+              descripcion: e['Descripcion'] as String,
+              tipoActividadId: e['TipoActividadId'] as int,
+              fechaCreacion: DateTime.parse(e['FechaCreacion'] as String),
+              fechaLimite: DateTime.parse(e['FechaLimite'] as String),
+              materiaId: e['MateriaId'] as int),
+        )
+        .toList();
+
+    return lsActivities;
+  }
 }
