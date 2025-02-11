b8073b9257871641e60e66fc6c54f3c37f2ec122
BotitaDLalo <canciondfuego0@gmail.com>
2025-02-11 13:40:00 -0600

    feat(providers/groups): Separa lógica para estudiantes del grupo e implementa en nuevos archivos.

--- lib/providers/groups/form_groups_provider.dart
+++ lib/providers/groups/form_groups_provider.dart
@@ -2,6 +2,7 @@ import 'package:aprende_mas/config/utils/packages.dart';
 import 'package:aprende_mas/providers/groups/form_groups_state.dart';
 import 'package:aprende_mas/providers/groups/form_groups_state_notifier.dart';
 import 'package:aprende_mas/providers/groups/groups_provider.dart';
+import 'package:aprende_mas/providers/groups/students_group_provider.dart';
 
 final formGroupsProvider =
     StateNotifierProvider.autoDispose<FormGroupsStateNotifier, FormGroupsState>(
@@ -9,9 +10,10 @@ final formGroupsProvider =
   final createGroupSubjectsCallback =
       ref.read(groupsProvider.notifier).createGroupSubjects;
   final updateGroupCallback = ref.read(groupsProvider.notifier).updateGroup;
-  final verifyEmailCallback = ref.read(groupsProvider.notifier).verifyEmail;
+  final verifyEmailCallback =
+      ref.read(studentsGroupProvider.notifier).verifyEmail;
   final addStudentsGroupCallback =
-      ref.read(groupsProvider.notifier).addStudentsGroup;
+      ref.read(studentsGroupProvider.notifier).addStudentsGroup;
   return FormGroupsStateNotifier(
     createGroupSubjectsCallback: createGroupSubjectsCallback,
     updateGroupCallback: updateGroupCallback,
