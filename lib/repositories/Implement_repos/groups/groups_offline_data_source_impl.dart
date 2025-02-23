6a151d0ec55ad3288f9188d0da251a4ffad529e5
BotitaDLalo <canciondfuego0@gmail.com>
2025-02-22 20:17:41 -0600

    feat(repositories/groups): Descarta campo color en grupos

--- lib/repositories/Implement_repos/groups/groups_offline_data_source_impl.dart
+++ lib/repositories/Implement_repos/groups/groups_offline_data_source_impl.dart
@@ -1,94 +1,93 @@
 import 'package:aprende_mas/models/models.dart';
 import 'package:aprende_mas/repositories/Interface_repos/groups/groups_offline_data_source.dart';
 import 'package:aprende_mas/config/data/db_local.dart';
+import 'package:aprende_mas/config/utils/general_utils.dart';
 
 class GroupsOfflineDataSourceImpl implements GroupsOfflineDataSource {
-@override
-Future<List<Group>> getGroupsSubjects() async {
-  try {
-    final db = await DbLocal.initDatabase();
+  @override
+  Future<List<Group>> getGroupsSubjects() async {
+    try {
+      final db = await DbLocal.initDatabase();
 
-    if (db.isOpen) {
-      final querylsGroups = await db.rawQuery('SELECT * FROM tbGrupos');
+      if (db.isOpen) {
+        final querylsGroups = await db.rawQuery('SELECT * FROM tbGrupos');
 
-      if (querylsGroups.isNotEmpty) {
-        List<Group> lsGroups = [];
+        if (querylsGroups.isNotEmpty) {
+          List<Group> lsGroups = [];
 
-        for (var groupRow in querylsGroups) {
-          int groupId = groupRow['GrupoId'] as int;
+          for (var groupRow in querylsGroups) {
+            int groupId = groupRow['GrupoId'] as int;
 
-          final querylsGroupsSubjectsId = await db.rawQuery(
-              "SELECT MateriaId FROM tbGruposMaterias WHERE GrupoId = ?",
-              [groupId]);
+            final querylsGroupsSubjectsId = await db.rawQuery(
+                "SELECT MateriaId FROM tbGruposMaterias WHERE GrupoId = ?",
+                [groupId]);
 
-          List<Subject> materias = [];
+            List<Subject> materias = [];
 
-          for (var subjectRow in querylsGroupsSubjectsId) {
-            int subjectId = subjectRow['MateriaId'] as int;
+            for (var subjectRow in querylsGroupsSubjectsId) {
+              int subjectId = subjectRow['MateriaId'] as int;
 
-            final querySubject = await db.rawQuery(
-                "SELECT * FROM tbMaterias WHERE MateriaId = ?", [subjectId]);
+              final querySubject = await db.rawQuery(
+                  "SELECT * FROM tbMaterias WHERE MateriaId = ?", [subjectId]);
 
-            final querylsActivitiesId = await db.rawQuery(
-                "SELECT ActividadId FROM tbMateriasActividades WHERE MateriaId = ?",
-                [subjectId]);
+              final querylsActivitiesId = await db.rawQuery(
+                  "SELECT ActividadId FROM tbMateriasActividades WHERE MateriaId = ?",
+                  [subjectId]);
 
-            List<Activity> actividades = [];
+              List<Activity> actividades = [];
 
-            for (var activityRow in querylsActivitiesId) {
-              int activityId = activityRow['ActividadId'] as int;
+              for (var activityRow in querylsActivitiesId) {
+                int activityId = activityRow['ActividadId'] as int;
 
-              final queryActivitie = await db.rawQuery(
-                  "SELECT * FROM tbActividades WHERE ActividadId = ?",
-                  [activityId]);
+                final queryActivitie = await db.rawQuery(
+                    "SELECT * FROM tbActividades WHERE ActividadId = ?",
+                    [activityId]);
 
-              actividades.add(Activity(
-                actividadId: queryActivitie[0]['ActividadId'] as int,
-                nombreActividad:
-                    queryActivitie[0]['NombreActividad'] as String,
-                descripcion: queryActivitie[0]['Descripcion'] as String,
-                tipoActividadId: queryActivitie[0]['TipoActividadId'] as int,
-                fechaCreacion: DateTime.parse(
-                    queryActivitie[0]['FechaCreacion'] as String),
-                fechaLimite: DateTime.parse(
-                    queryActivitie[0]['FechaLimite'] as String),
-                materiaId: queryActivitie[0]['MateriaId'] as int,
+                actividades.add(Activity(
+                    actividadId: queryActivitie[0]['ActividadId'] as int,
+                    nombreActividad:
+                        queryActivitie[0]['NombreActividad'] as String,
+                    descripcion: queryActivitie[0]['Descripcion'] as String,
+                    tipoActividadId:
+                        queryActivitie[0]['TipoActividadId'] as int,
+                    fechaCreacion: formatDate(
+                        queryActivitie[0]['FechaCreacion'] as String),
+                    fechaLimite:
+                        formatDate(queryActivitie[0]['FechaLimite'] as String),
+                    materiaId: queryActivitie[0]['MateriaId'] as int,
+                    puntaje: queryActivitie[0]['Puntaje'] as int));
+              }
+
+              materias.add(Subject(
+                materiaId: subjectId,
+                nombreMateria: querySubject[0]['NombreMateria'] as String,
+                descripcion: querySubject[0]['Descripcion'] as String,
+                codigoAcceso: querySubject[0]['CodigoAcceso'] as String,
+                actividades: actividades,
               ));
             }
 
-            materias.add(Subject(
-              materiaId: subjectId,
-              nombreMateria: querySubject[0]['NombreMateria'] as String,
-              descripcion: querySubject[0]['Descripcion'] as String,
-              codigoAcceso: querySubject[0]['CodigoAcceso'] as String,
-              codigoColor: querySubject[0]['CodigoColor'] as String,
-              actividades: actividades,
+            lsGroups.add(Group(
+              grupoId: groupRow['GrupoId'] as int,
+              nombreGrupo: groupRow['NombreGrupo'] as String,
+              descripcion: groupRow['Descripcion'] as String,
+              codigoAcceso: groupRow['CodigoAcceso'] as String,
+              materias: materias,
             ));
           }
 
-          lsGroups.add(Group(
-            grupoId: groupRow['GrupoId'] as int,
-            nombreGrupo: groupRow['NombreGrupo'] as String,
-            descripcion: groupRow['Descripcion'] as String,
-            codigoAcceso: groupRow['CodigoAcceso']  as String,
-            codigoColor: groupRow['CodigoColor'] as String,
-            materias: materias,
-          ));
+          await db.close();
+          return lsGroups;
         }
-
-        await db.close();
-        return lsGroups;
       }
+
+      await db.close();
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
     }
-
-    await db.close();
-    return [];
-  } catch (e) {
-    print(e);
-    return [];
   }
-}
-
 
   @override
   Future<void> saveGroupSubjects(List<Group> lsGroups) async {
@@ -102,8 +101,7 @@ Future<List<Group>> getGroupsSubjects() async {
                 'GrupoId': group.grupoId,
                 'NombreGrupo': group.nombreGrupo,
                 'Descripcion': group.descripcion,
-                'CodigoAcceso': group.codigoAcceso,
-                'CodigoColor': group.codigoColor
+                'CodigoAcceso': group.codigoAcceso
               });
             },
           );
@@ -116,7 +114,6 @@ Future<List<Group>> getGroupsSubjects() async {
                 'MateriaId': subject.materiaId,
                 'NombreMateria': subject.nombreMateria,
                 'Descripcion': subject.descripcion,
-                'CodigoColor': subject.codigoColor,
                 'CodigoAcceso': subject.codigoAcceso
               });
 
@@ -134,7 +131,8 @@ Future<List<Group>> getGroupsSubjects() async {
                     'Descripcion': activity.descripcion,
                     'FechaCreacion': activity.fechaCreacion.toString(),
                     'FechaLimite': activity.fechaLimite.toString(),
-                    'MateriaId': subjectId
+                    'MateriaId': subjectId,
+                    'Puntaje': activity.puntaje
                   });
 
                   await db.insert('tbMateriasActividades', {
@@ -146,8 +144,8 @@ Future<List<Group>> getGroupsSubjects() async {
             }
           }
         }
+        await db.close();
       }
-      await db.close();
     } catch (e) {
       print(e);
     }
