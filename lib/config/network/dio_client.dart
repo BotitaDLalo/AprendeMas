678a20c20116ecec0ae6b9ed003c1eb7254e58a0
BotitaDLalo <canciondfuego0@gmail.com>
2025-03-02 09:44:24 -0600

    feat(config/network): Modifica duración para establecer conexión con el servidor.

--- lib/config/network/dio_client.dart
+++ lib/config/network/dio_client.dart
@@ -1,4 +1,7 @@
 import 'package:aprende_mas/config/environment/environment.dart';
 import 'package:dio/dio.dart';
 
-final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+final dio = Dio(BaseOptions(
+  baseUrl: Environment.apiUrl,
+  connectTimeout: const Duration(seconds: 30),
+));
