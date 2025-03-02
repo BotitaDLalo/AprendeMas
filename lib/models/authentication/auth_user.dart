41b233b7f203f28d5ea71720cdc54f5509cc7834
BotitaDLalo <canciondfuego0@gmail.com>
2025-03-02 09:31:45 -0600

    feat(models/authentication): Modifica modelos para autenticación

--- lib/models/authentication/auth_user.dart
+++ lib/models/authentication/auth_user.dart
@@ -4,13 +4,15 @@ class AuthUser {
   final String email;
   final String role;
   final String token;
+  final String? estaAutorizado;
 
   AuthUser(
       {required this.userId,
       required this.userName,
       required this.email,
       required this.role,
-      required this.token});
+      required this.token,
+      this.estaAutorizado});
 
   static AuthUser authUserVoid() =>
       AuthUser(userId: -1, userName: '', email: '', role: '', token: '');
--- lib/models/authentication/user_mapper.dart
+++ lib/models/authentication/user_mapper.dart
@@ -1,7 +1,8 @@
 import 'package:aprende_mas/models/models.dart';
 
 class UserMapper {
-  static User userSiginJsonToEntity(Map<String, dynamic> json) =>
-      User(
-          nombre: json['nombre'], email: json['correo'], rol: json['rol'].toString());
+  static User userSiginJsonToEntity(Map<String, dynamic> json) => User(
+      nombre: json['nombre'],
+      email: json['correo'],
+      rol: json['rol'].toString());
 }
