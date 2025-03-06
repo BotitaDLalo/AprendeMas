4fc8b802b4e1d814512764cadbed751199782ae9
BotitaDLalo <canciondfuego0@gmail.com>
2025-03-06 16:48:33 -0600

    feat(models/authentication): Agrega propiedad  requiereDatosAdicionales en método mapper

--- lib/models/authentication/auth_user_mapper.dart
+++ lib/models/authentication/auth_user_mapper.dart
@@ -2,10 +2,12 @@ import 'package:aprende_mas/models/models.dart';
 
 class AuthUserMapper {
   static AuthUser userJsonToEntity(Map<String, dynamic> user) => AuthUser(
-      userId: user['id'] ?? -1,
-      userName: user['userName'] ?? "",
-      email: user['correo'] ?? "",
-      role: user['rol'] ?? "",
-      token: user['token'] ?? "",
-      estaAutorizado: user['estaAutorizado'] ?? "");
+        userId: user['id'] ?? -1,
+        userName: user['userName'] ?? "",
+        email: user['correo'] ?? "",
+        role: user['rol'] ?? "",
+        token: user['token'] ?? "",
+        estaAutorizado: user['estaAutorizado'] ?? "",
+        requiereDatosAdicionales: user['requiereDatosAdicionales'] ?? false,
+      );
 }
