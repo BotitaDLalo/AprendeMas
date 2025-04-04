53a8897e64ed53d584c690f139359c1daa233023
BotitaDLalo <canciondfuego0@gmail.com>
2025-04-03 21:51:29 -0600

    feat(models/various): Crea metodo mapper para respuesta Join Class

--- lib/models/various/join_class.dart
+++ lib/models/various/join_class.dart
@@ -8,7 +8,7 @@ class JoinClass {
   JoinClass(
       {required this.group, required this.subject, required this.isGroup});
 
-  static JoinClass resJsonToEntity(Map<String, dynamic> map) => JoinClass(
+  static JoinClass  resJsonToEntity(Map<String, dynamic> map) => JoinClass(
       group: map['grupo'] != null ? Group.mapToEntity(map['grupo']): null,
       subject: map['materia'] != null ? Subject.mapToEntity(map['materia']): null,
       isGroup: map['esGrupo'] as bool);
